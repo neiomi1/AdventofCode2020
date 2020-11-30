c0ed0288e493732097a2463c8e241222ae19b591
neiomi1 <48922182+neiomi1@users.noreply.github.com>
2020-11-30 10:17:37 +0100

    Add project files.

---
+++ AdventofCodeDay1/AdventofCodeDay1.cpp
@@ -0,0 +1,12 @@
+﻿// AdventofCodeDay1.cpp : Defines the entry point for the application.
+//
+
+#include "AdventofCodeDay1.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
