d6c56aa9604f8a62e69e875160dbd2e7f20dd62f
neiomi1 <48922182+neiomi1@users.noreply.github.com>
2020-12-01 10:09:39 +0100

    Day_01 timers added

--- Day_01/Day_01.cpp
+++ Day_01/Day_01.cpp
@@ -5,6 +5,7 @@
 #include <vector>
 #include <fstream>
 #include <algorithm>
+#include <chrono>
 
 int num_entries = 0;
 std::vector<int> entries;
@@ -55,7 +56,23 @@ int find_triple(int sum_goal) {
 
 int main()
 {
+	auto file_time = std::ofstream{ "..\\..\\..\\..\\Day_01\\execution_times.txt" };
+	auto start = std::chrono::high_resolution_clock::now();
 	read_entries("input.txt");
+	auto stop = std::chrono::high_resolution_clock::now();
+	auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+	file_time << "Input read in " << duration.count() << " microseconds.\n";
+
+	start = std::chrono::high_resolution_clock::now();
 	std::cout << "Pair Multiplication: " << find_pair(2020) << "\n";
+	stop = std::chrono::high_resolution_clock::now();
+	duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+	file_time << "Pair calculated in " << duration.count() << " microseconds.\n";
+
+	start = std::chrono::high_resolution_clock::now();
 	std::cout << "Triple Multiplcation: " << find_triple(2020) << "\n";
+	stop = std::chrono::high_resolution_clock::now();
+	duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+	file_time << "Triple calculated in " << duration.count() << " microseconds.";
+
 }
