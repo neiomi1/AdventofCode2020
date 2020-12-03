565866c1703c4b2647dbd42894df1b5d93e5bd14
neiomi1 <48922182+neiomi1@users.noreply.github.com>
2020-12-03 10:08:20 +0100

    Day_03 complete

---
+++ Day_03/Day_03.h
@@ -0,0 +1,17 @@
+﻿// AdventofCodeDay1.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+#include <istream>
+#include<fstream>
+#include<unordered_map>
+
+// TODO: Reference additional headers your program requires here.
+
+struct Tree_Biome {
+	int size;
+	std::unordered_map<int, std::unordered_map<int, bool>> tree_map;
+	std::unordered_map<int, int> repeating_pattern;
+};
