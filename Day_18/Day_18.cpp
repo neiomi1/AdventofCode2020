ef763bb6c5dc743949dd64900f75b92c96eac365
neiomi1 <48922182+neiomi1@users.noreply.github.com>
2020-12-19 12:23:45 +0100

    Day_19 help :(

---
+++ Day_18/Day_18.cpp
@@ -0,0 +1,20 @@
+﻿#include <sstream>
+#include "Day_18.h"
+
+Calculator::Calculator(const std::string& input):input_(input)
+{
+}
+
+uint64_t Calculator::calculate(int pos)
+{
+	uint64_t result = 0;
+	bool num = false;
+	for (int i = pos; i < std::ssize(input_); i++) {
+		if (input_[i] >= '0' && input_[i] <= '9') {
+			if (!num) {
+				result = input_[i] - '0';
+			}
+		}
+	}
+	return 0;
+}
---
+++ Day_19/Day_19.cpp
@@ -0,0 +1,92 @@
+﻿#include <sstream>
+#include "Day_19.h"
+
+Simple_Rule::Simple_Rule(const char& letter):letter_(letter){}
+
+bool Simple_Rule::match(const std::string& input, int index)
+{
+	return input[index] == letter_;
+}
+
+Combine_Rule::Combine_Rule(std::unordered_map<int, std::unique_ptr<Rule>>& map,std::vector<int>& list): map_(map){
+	add_rule(list);
+}
+
+void Combine_Rule::add_rule(std::vector<int>& list)
+{
+	std::vector<int> ruleset;
+	for (auto it = list.begin(); it != list.end(); it++) {
+		ruleset.push_back(*it);
+	}
+	rules_.push_back(ruleset);
+}
+
+bool Combine_Rule::match(const std::string& input, int index)
+{
+	for (auto ruleset : rules_) {
+		int ind = index;
+		bool matches = true;
+		for (auto rule : ruleset) {
+			matches = matches && (*map_[rule]).match(input, ind);
+		}
+		if (matches) {
+			return true;
+		}
+	}
+	return false;
+}
+
+Messages::Messages(const std::string& filepath) : filepath_(filepath)
+{
+}
+
+void Messages::parse()
+{
+	auto file = std::ifstream{ filepath_ };
+	int rulenum;
+	std::string line;
+	while (std::getline(file, line)) {
+		if (line.find(':') != std::string::npos) {
+			int ruleint;
+			char c;
+			auto stream = std::istringstream{ line };
+			stream >> rulenum >> c;
+			std::vector<int> rules;
+			while (stream >> ruleint) {
+				rules.push_back(ruleint);
+			}
+			if (rules.empty()) {
+				stream >> c;
+				std::cout << rulenum << " " << c << "\n";
+				rules_[rulenum] = std::make_unique<Rule>(Simple_Rule(c));
+			}
+			else {
+				auto rule = Combine_Rule(rules_, rules);
+				while (!rules.empty()) {
+					rules.clear();
+					while (stream >> ruleint) {
+						rules.push_back(ruleint);
+					}
+					rule.add_rule(rules);
+				}
+				rules_[rulenum] = std::make_unique<Rule>(rule);
+			}
+
+
+		}
+		else {
+			input_.push_back(line);
+		}
+	}
+}
+
+int Messages::part_one(int starting_rule)
+{
+	int sum = 0;
+	for (const auto& line : input_) {
+		if ((*rules_[0]).match(line, starting_rule)) {
+			sum++;
+		}
+	}
+	return sum;
+}
